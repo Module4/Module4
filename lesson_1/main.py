3c1b2065d29c677d7b0b6fd3bfcd1bf5c6ca0631
Данил <mysc1@yandex.ru>
2023-04-11 10:50:29 +0300

    Занятие №1. Код.

---
+++ lesson_1/main.py
@@ -0,0 +1,28 @@
+# def strcounter(s): #решение за N**2
+#     for sym in s:
+#         counter = 0
+#         for sub_sym in s:
+#             if sym == sub_sym:
+#                 counter += 1
+#         print(sym, counter)
+
+# strcounter('aabbbbccd')
+
+# def strcounter(s): # решение за N * M
+#     for sym in set(s):
+#         counter = 0
+#         for sub_sym in s:
+#             if sym == sub_sym:
+#                 counter += 1
+#         print(sym, counter)
+
+# strcounter('aabbbbccd')
+
+def strcounter(s): # решение за N
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+
+    for sym, count in syms_counter.items():
+        print(sym, count)
+strcounter('aabbbbccd')
