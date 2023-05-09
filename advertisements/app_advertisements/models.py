7a312d24902e742f06e9d6315aae0e5fe92ae9b3
Данил <mysc1@yandex.ru>
2023-05-09 20:47:37 +0300

    Занятие №8. Код с урока.

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
 
 class Advertisement(models.Model):
@@ -6,8 +8,18 @@ class Advertisement(models.Model):
     description = models.TextField("описание")
     price = models.DecimalField("цена", max_digits=10, decimal_places=2)
     auction = models.BooleanField("торг", help_text="Отметьте, если торг уместен")
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description='дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
