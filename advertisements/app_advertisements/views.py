7771b5ad05b831852295ee858e75aa0699156f05
Данил <mysc1@yandex.ru>
2023-05-19 21:50:51 +0300

    Занятие №10. ДЗ.

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -18,23 +18,12 @@ def advertisement_post(request):
     if request.method == "POST":
         form = AdvertisementForm(request.POST, request.FILES)
         if form.is_valid():
-            advertisement = Advertisement(**form.cleaned_data)
-            advertisement.user = request.user
-            advertisement.save()
+            new_advertisement = form.save(commit=False)
+            new_advertisement.user = request.user
+            new_advertisement.save()
             url = reverse('main-page')
             return redirect(url)
     else:
         form = AdvertisementForm()
     context = {'form': form}
     return render(request, 'advertisement-post.html', context)
-
-# def advertisement_post(request):
-#     if request.method == "POST":
-#         form = AdvertisementForm(request.POST)
-#         if form.is_valid():
-#             url = reverse('app_advertisement:adv-post')
-#             return redirect(url)
-#     else:
-#         form = AdvertisementForm()
-#     context = {'form': form}
-#     return render(request, 'advertisement-post.html', context)
