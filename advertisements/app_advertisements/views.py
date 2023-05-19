58e887849dfe7438971aad915a008a4f1f705973
Данил <mysc1@yandex.ru>
2023-05-19 21:00:42 +0300

    Занятие №10. Код с урока.

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from .models import Advertisement
+from .forms import AdvertisementForm
 
 
 def index(request):
@@ -10,3 +12,29 @@ def index(request):
 
 def top_sellers(request):
     return render(request, 'top-sellers.html')
+
+
+def advertisement_post(request):
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement = Advertisement(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
+            url = reverse('main-page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+    context = {'form': form}
+    return render(request, 'advertisement-post.html', context)
+
+# def advertisement_post(request):
+#     if request.method == "POST":
+#         form = AdvertisementForm(request.POST)
+#         if form.is_valid():
+#             url = reverse('app_advertisement:adv-post')
+#             return redirect(url)
+#     else:
+#         form = AdvertisementForm()
+#     context = {'form': form}
+#     return render(request, 'advertisement-post.html', context)
