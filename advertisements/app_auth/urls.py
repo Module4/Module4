30072ca2e96f728d50cae08785e2ae96b90de03e
Данил <mysc1@yandex.ru>
2023-05-20 23:48:39 +0300

    Занятие №11. ДЗ.

--- advertisements/app_auth/urls.py
+++ advertisements/app_auth/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import login_view, profile_view, logout_view
+from .views import login_view, profile_view, logout_view, register_view
 
 urlpatterns = [
     path("login/", login_view, name='login'),
     path("profile/", profile_view, name="profile"),
     path("logout/", logout_view, name='logout'),
+    path('register/', register_view, name='register'),
 ]
