eaa4843e96052f606aa26f7f15f502c2f4cd8fbb
Данил <mysc1@yandex.ru>
2023-05-22 22:17:59 +0300

    Занятие №12. Код с урока

--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, top_sellers, advertisement_post
+from .views import index, top_sellers, advertisement_post, advertisement_detail
 
 urlpatterns = [
     path('', index, name='main-page'),
     path('top-sellers/', top_sellers, name='top-sellers'),
     path('advertisement-post/', advertisement_post, name='adv-post'),
+    path('advertisement/<int:pk>', advertisement_detail, name='adv-detail')
 ]
--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -1,18 +1,46 @@
+from django.contrib.auth import get_user_model
+from django.db.models import Count
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 from .models import Advertisement
 from .forms import AdvertisementForm
 from django.contrib.auth.decorators import login_required
 
+User = get_user_model()
+
 
 def index(request):
-    advertisements = Advertisement.objects.all()
-    context = {'advertisements': advertisements}
+    title = request.GET.get('query')
+    if title:
+        advertisements = Advertisement.objects.filter(title__icontains=title)
+    else:
+        advertisements = Advertisement.objects.all()
+
+    context = {
+        'advertisements': advertisements,
+        'title': title,
+    }
     return render(request, 'app_advertisements/index.html', context)
 
 
+def advertisement_detail(request, pk):
+    advertisement = Advertisement.objects.get(id=pk)
+    context = {
+        'advertisement': advertisement,
+    }
+    return render(request, 'app_advertisements/advertisement.html', context)
+
+
 def top_sellers(request):
-    return render(request, 'app_advertisements/top-sellers.html')
+    users = User.objects.annotate(
+        adv_count=Count('advertisement')
+    ).order_by('-adv_count')
+
+    context = {
+        'users': users,
+    }
+
+    return render(request, 'app_advertisements/top-sellers.html', context)
 
 
 @login_required(login_url=reverse_lazy('login'))
